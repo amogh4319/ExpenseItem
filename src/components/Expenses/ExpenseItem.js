2454cab60aab14477a5d9a676d8ca218eee9c60f
amogh4319 <amogh4319@gmail.com>
2023-07-18 00:20:44 +0530

    adding change title ,new amt button

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,21 +1,36 @@
-import React from 'react';
+import React,{useState} from 'react';
 import './ExpenseItem.css'
 import ExpenseDate from './ExpenseDate';
 import ExpenseDetails from './ExpenseDetails';
 import Card from '../UI/Card';
+
 const ExpenseItem=(props)=>{
     function deleteExpense(target){
         const expenseItem=target.parentElement;
         const expense=document.querySelector('.expenses');
         expense.removeChild(expenseItem);
     }
+
+    const[title,setTitle]=useState(props.title);
+    function clickHandler(){
+        setTitle('updated!');
+        console.log(title);
+    }
+    const[amount,setAmount]=useState(props.amount);
+    function amtHandler(){
+        setAmount('100');
+        console.log(amount);
+    }
    
     return (
         <Card className="expense-item">
             <ExpenseDate date={props.date}></ExpenseDate>
             
-           <ExpenseDetails  amount={props.amount} place={props.place} title={props.title}/>
+           <ExpenseDetails  amount={amount} place={props.place} title={title}/>
+           <button onClick={amtHandler}>new amt</button>
            <button onClick={(e)=>deleteExpense(e.target)}>Delete Expenses</button>
+           <button onClick={clickHandler}>Change title</button>
+           
         </Card>
     
 
