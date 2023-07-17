7808675b5b751e20cd5c5bb50765a84af39572a0
amogh4319 <amogh4319@gmail.com>
2023-07-17 22:43:25 +0530

    adding delete button

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -4,12 +4,18 @@ import ExpenseDate from './ExpenseDate';
 import ExpenseDetails from './ExpenseDetails';
 import Card from '../UI/Card';
 const ExpenseItem=(props)=>{
-    
+    function deleteExpense(target){
+        const expenseItem=target.parentElement;
+        const expense=document.querySelector('.expenses');
+        expense.removeChild(expenseItem);
+    }
+   
     return (
         <Card className="expense-item">
             <ExpenseDate date={props.date}></ExpenseDate>
             
            <ExpenseDetails  amount={props.amount} place={props.place} title={props.title}/>
+           <button onClick={(e)=>deleteExpense(e.target)}>Delete Expenses</button>
         </Card>
     
 
