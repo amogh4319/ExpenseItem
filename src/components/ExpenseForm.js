71579fd1d82aaa11594076df9e2b61bb8bbb6c3a
amogh4319 <amogh4319@gmail.com>
2023-07-18 19:53:33 +0530

    adding useState to title,amount,date

--- src/components/ExpenseForm.js
+++ src/components/ExpenseForm.js
@@ -1,8 +1,22 @@
+import React,{useState} from 'react';
 import './ExpenseForm.css';
 function ExpenseForm(props){
+const [title,enteredTitle]=useState('');
     const changeTitleHandler=(event)=>{
-        console.log(event.target.value);
+        enteredTitle(event.target.value);
+        console.log(title);
     }
+    const [amount,enteredAmount]=useState('');
+    const changeAmtHandler=(event)=>{
+        enteredAmount(event.target.value);
+        console.log(amount);
+    }
+    const [date,enteredDate]=useState('');
+    const changeDateHandler=(event)=>{
+        enteredDate(event.target.value);
+        console.log(date);
+    }
+    
    
     return(
         
@@ -16,12 +30,12 @@ function ExpenseForm(props){
                 
                 <div className='new-expense__control'>
                     <label>Amount</label>
-                    <input type="number" min="0.01" step="0.01"/>
+                    <input type="number" min="0.01" step="0.01" onChange={changeAmtHandler}/>
                 </div>
                 
                 <div className='new-expense__control'>
                     <label>Date</label>
-                    <input type="date" min="2019-01-01" max="2022-12-31"/>
+                    <input type="date" min="2019-01-01" max="2022-12-31" onChange={changeDateHandler}/>
                 </div>
                
                 
