233737ea33f16ff5cb3f8ede20aba18d8f1f574d
amogh4319 <amogh4319@gmail.com>
2023-07-19 20:43:09 +0530

    updating list with new item and using key

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import NewExpenses from './components/NewExpenses';
 import React,{useState} from 'react';
 import Expenses from './components/Expenses/Expenses';
-const App=()=> {
-  const [expenses,setExpenses] = useState([
+
+  const DUMMY_EXPENSES=[
     {
       id: 'e1',
       title: 'Toilet Paper',
@@ -31,12 +31,17 @@ const App=()=> {
       date: new Date(2021, 5, 12),
       place:'chennai',
     },
-  ]);
-  const addExpenseHandler=(expenseData)=>{
-    setExpenses((expenses)=>[expenseData,...expenses]
+  ];
+
+  const App=()=> {
+    const [expenses,setExpenses]=useState(DUMMY_EXPENSES);
+  const addExpenseHandler=(expense)=>{
+    setExpenses((prevExpense)=>{
+      return [expense,...prevExpense];
+    })
       
-    )
-  }
+    
+  };
   return (
     <div>
       
--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -12,33 +12,16 @@ const Expenses=(props)=>{
 return(
     <Card className="expenses">
         <ExpenseFilter selected={filteredYear} onChangeFilter={filterChangeHandler}/>
+        {props.items.map((expense)=>(
         <ExpenseItem
-    title={props.items[0].title}
-    amount={props.items[0].amount}
-    date={props.items[0].date}
-    place={props.items[0].place}
-    ></ExpenseItem>
+            key={expense.id}
+            title={expense.title}
+            amount={expense.amount}
+            date={expense.date}
+            ></ExpenseItem>
 
-    <ExpenseItem
-    title={props.items[1].title}
-    amount={props.items[1].amount}
-    date={props.items[1].date}
-    place={props.items[1].place}
-    ></ExpenseItem>
+        ))}
 
-    <ExpenseItem
-    title={props.items[2].title}
-    amount={props.items[2].amount}
-    date={props.items[2].date}
-    place={props.items[2].place}
-    ></ExpenseItem>
-
-    <ExpenseItem
-    title={props.items[3].title}
-    amount={props.items[3].amount}
-    date={props.items[3].date}
-    place={props.items[3].place}
-    ></ExpenseItem>
     </Card>
     
     
